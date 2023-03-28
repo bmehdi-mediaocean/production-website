92bf33de3541b68b178acb25e16d2fc2b45a310b
BASIM MEHDI <bmehdi@mediaocean.com>
2023-03-28 13:56:50 +0100

    Styling adjustments and form populating

--- src/components/BriefingForm.js
+++ src/components/BriefingForm.js
@@ -11,16 +11,36 @@ class BriefingForm extends React.Component {
                     <h1>Briefing Form</h1>
                     <div className={styles.forms}>
                         <h2>Project Details</h2>
-                        <div className={styles.halfSections}>
+                        <div className={styles.halfSections + ' ' + styles.leftSections}>
                             <label htmlFor='projectTitle'>Project Title</label>
-                            <input type='text' name='projectTitle' placeholder='Enter Project Title' />
+                            <input
+                                type='text'
+                                name='projectTitle'
+                                placeholder='Enter Project Title'
+                            />
                         </div>
-                        <div className={styles.halfSections}>
+                        <div className={styles.halfSections + ' ' + styles.rightSections}>
                             <label htmlFor='campaignLiveDate'>Campaign Live Date</label>
                             <input
                                 type='date'
                                 name='campaignLiveDate'
-                                placeholder='DD-MM-YYYY' />
+                                placeholder='DD-MM-YYYY'
+                            />
+                        </div>
+                        <div className={styles.halfSections + ' ' + styles.leftSections}>
+                            <label htmlFor='fullName'>Full Name</label>
+                            <input
+                                className={styles.halfInput + ' ' + styles.leftInput}
+                                type='text'
+                                name='firstName'
+                                placeholder='First Name'
+                            />
+                            <input
+                                className={styles.halfInput + ' ' + styles.rightInput}
+                                type='text'
+                                name='lastName'
+                                placeholder='Last Name'
+                            />
                         </div>
                     </div>
                 </div>
